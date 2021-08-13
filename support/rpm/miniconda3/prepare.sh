1f1a5e2582088fb40a69b410fbe540a0d5dd278d
Peter Willendrup <pkwi@fysik.dtu.dk>
2021-08-13 06:20:18 -0400

    Changes needed for succesful Scientific Linux 7.9 build

--- support/rpm/miniconda3/prepare.sh
+++ support/rpm/miniconda3/prepare.sh
@@ -10,8 +10,8 @@ chmod a+x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda3/
 export PATHBAK=$PATH
 export PATH=$PWD/miniconda3/bin:$PATH
-$PWD/miniconda3/bin/conda install libxkbcommon
-$PWD/miniconda3/bin/pip install PyQt5 Qscintilla pyqtgraph pyaml ply matplotlib numpy tornado scipy pillow jinja2 mpld3
+$PWD/miniconda3/bin/conda install matplotlib numpy scipy pillow nomkl -y
+$PWD/miniconda3/bin/pip install PyQt5 Qscintilla pyqtgraph pyaml ply tornado jinja2 mpld3
 export PATH=$PATHBAK
 #pick a flavor
 if [ "mcxtrace" == "$2" ]; then
